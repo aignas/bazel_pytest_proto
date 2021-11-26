14c809f6229a33fbbce55ade376b541369506300
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2021-11-26 11:25:27 +0200

    update the wrapper

--- tools/pytest_wrapper.py
+++ tools/pytest_wrapper.py
@@ -1,8 +1,5 @@
-import sys
-
-import pytest
-
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    print("Got args {}".format(args))
-    sys.exit(pytest.main(args))
+    import sys
+
+    import pytest
+    sys.exit(pytest.main(sys.argv[1:]))
