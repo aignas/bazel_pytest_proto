d06c213fc3f3b892b9091ea1f65c54c24f47c32f
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2021-11-26 11:42:53 +0200

    import-mode needs to be append, because bazel is taking care of everything

--- lib/foo/pytest_runner/main_test.py
+++ lib/foo/pytest_runner/main_test.py
@@ -1,8 +1,4 @@
-# This import must be here or otherwise pytest changes our env and this import
-# path stops working due to some reason.
-import idl.foo.foo_pb2
-
 if __name__ == "__main__":
     import sys
     import pytest
-    pytest.main(sys.argv[1:])
+    pytest.main(sys.argv[1:] + ["--import-mode=append"])
